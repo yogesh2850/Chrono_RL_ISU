23de2bb2f7f959e475de9a2af39a7da130ebdbb2
Yogesh Chawla <yogeshchawla2850@gmail.com>
2022-06-27 15:03:48 -0500

    Update final.py

--- final.py
+++ final.py
@@ -36,7 +36,6 @@ my_system = chrono.ChSystemSMC()
 
 # Create a mesh
 mesh = fea.ChMesh()
-my_system.Add(mesh)
 
 ## Create a mesh, that is a container for groups
 ## of elements and their referenced nodes.
@@ -47,14 +46,17 @@ nodeB = fea.ChNodeFEAxyzrot(chrono.ChFrameD(chrono.ChVectorD(1, 0, 0)))
 nodeC = fea.ChNodeFEAxyzrot(chrono.ChFrameD(chrono.ChVectorD(0, 1, 0)))
 nodeD = fea.ChNodeFEAxyzrot(chrono.ChFrameD(chrono.ChVectorD(0, 0, 1)))
 
-nodeA.SetYoungModulus(E1)
-nodeB.SetYoungModulus(E2)
-nodeA.SetDensity(density1)
-nodeB.SetDensity(density2)
-nodeA.SetPoissonRatio(v1)
-nodeB.SetPoissonRatio(v2)
-nodeA.SetBeamRaleyghDamping(dampingCoeff1)
-nodeB.SetBeamRaleyghDamping(dampingCoeff2)
+material1 = chrono.ChContinuumElastic
+material2 = chrono.ChContinuumElastic
+
+material1.SetYoungModulus(E1)
+material2.SetYoungModulus(E2)
+material1.SetDensity(density1)
+material2.SetDensity(density2)
+material1.SetPoissonRatio(v1)
+material2.SetPoissonRatio(v2)
+material1.SetBeamRaleyghDamping(dampingCoeff1)
+material2.SetBeamRaleyghDamping(dampingCoeff2)
 
 mesh.AddNode(nodeA)
 mesh.AddNode(nodeB)
@@ -62,7 +64,7 @@ mesh.AddNode(nodeC)
 mesh.AddNode(nodeD)
 
 element1= fea.ChElementTetrahedron()
-element2= fea.ChElementTetrahedron()
+#element2= fea.ChElementTetrahedron()
 
 
 element1.SetNodes(nodeA, nodeB, nodeC, nodeD)
@@ -70,9 +72,45 @@ element1.SetNodes(nodeA, nodeB, nodeC, nodeD)
 
 mesh.AddElement(element1)
 
+# #Shortcut (Link: https://api.projectchrono.org/group__fea__utils.html) but we don't have this for tetrahedron
+# builder = fea.ChBuilderBeamEuler()
+
+# # Now, simply use BuildBeam to create a plant from a point to another:
+# builder.BuildBeam(mesh,                   # the mesh where to put the created nodes and elements
+#                     5,                         # the number of ChElementBeamEuler to create
+#                     chrono.ChVectorD(0, 0, -0.1),   # the 'A' point in space (beginning of beam)
+#                     chrono.ChVectorD(0.2, 0, -0.1), # the 'B' point in space (end of beam)
+#                     chrono.ChVectorD(0, 1, 0))
+
+
+chrono.ChContinuumMaterial = {material1, material2}
+class BoundaryConstruct{
+    boundaryID, isDirichlet, forceValue[3]
+}
+
+BoundaryConstruct.boundaryID=boundaryFileName
+
 # We want gravity effect on FEA elements in this demo
 mesh.SetAutomaticGravity(True)
 my_system.Set_G_acc(chrono.ChVectorD(0,-9.81, 0))
 
+# Fix a node to ground:
+# node1.SetFixed(True)
+# otherwise fix it using constraints:
 
+mtruss = chrono.ChBody()
+mtruss.SetBodyFixed(True)
+my_system.Add(mtruss)
 
+# Remember to add the mesh to the system!
+my_system.Add(mesh)
+
+#Run the simulation for finalTime timesteps and save the data in .vtu files
+counter=0
+
+# while (my_system.GetChTime() < finalTime) {
+#         my_system.DoStepDynamics(timestep)
+#         if(writeVTU) {
+#             dumpData(my_mesh, "elastic" + counter + ".vtu");
+#         }
+#         counter+=counter;
